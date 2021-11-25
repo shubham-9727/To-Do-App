a029a639b63f76a0a1dbec701f11189458bf027b
shubham-9727 <shubhamgupta9727@gmail.com>
2021-11-26 01:49:21 +0530

    added server

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+var count = 1;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
